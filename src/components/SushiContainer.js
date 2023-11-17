e85c428bf3a18768fdbd6f210cee61f94c9938fc
Austin Oblow <austinoblow97@gmail.com>
2023-11-16 20:11:04 -0600

    first two deliverables passing

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer() {
+  const [sushi, setSushi] = useState([])
+  const [visibleSushi, setVisableSushi] = useState(4)
+
+  useEffect(() => {
+    fetch("http://localhost:3001/sushis")
+    .then(res => res.json())
+    .then(data => setSushi(data))
+  }, )
+
+  const handleShowSushi = () => {
+    setVisableSushi((lastSushi) => lastSushi + 4)
+  }
+
+
+  const sushiArr = sushi.slice(0, visibleSushi).map((sushiObj) => {
+    return <Sushi key={sushiObj.id} sushi={sushiObj}  />
+  })
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiArr}
+      <MoreButton onShowSushi={handleShowSushi} />
     </div>
   );
 }
